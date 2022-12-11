1b39b8aa0c0082327751b18358a6b92ca8fb2850
alexvenga <bubnov.alexey@gmail.com>
2022-12-11 19:47:33 +0300

    INIT

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # laravel-site-settings
 Easy Laravel settings (stored in MYSQL) package with MoonShine Laravel Admin GUI
+
+php artisan vendor:publish --provider="VI\LaravelSiteSettings\LaravelSiteSettingsProvider" --tag="config"
+
+php artisan vendor:publish --provider="VI\LaravelSiteSettings\LaravelSiteSettingsProvider" --tag="moonshine"
+
--- src/LaravelSiteSettingsProvider.php
+++ src/LaravelSiteSettingsProvider.php
@@ -9,11 +9,11 @@ class LaravelSiteSettingsProvider extends ServiceProvider
 
     public function register()
     {
-        $this->app->bind('LssConfig', function($app) {
+        $this->app->bind('LssConfig', function ($app) {
             return new LaravelSiteSettings();
         });
 
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'laravel_site_settings');
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'laravel_site_settings');
     }
 
     public function boot()
@@ -22,9 +22,13 @@ class LaravelSiteSettingsProvider extends ServiceProvider
         if ($this->app->runningInConsole()) {
 
             $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('laravel_site_settings.php'),
+                __DIR__ . '/../config/config.php' => config_path('laravel_site_settings.php'),
             ], 'config');
-            // php artisan vendor:publish --provider="VI\LaravelSiteSettings\LaravelSiteSettingsProvider" --tag="config"
+
+            $this->publishes([
+                __DIR__ . '/../stubs/MoonShine/Resources/LaravelSiteSettingGroupResource.php.stub' => app_path('MoonShine/Resources/LaravelSiteSettingGroupResource.php'),
+                __DIR__ . '/../stubs/MoonShine/Resources/LaravelSiteSettingResource.php.stub'      => app_path('MoonShine/Resources/LaravelSiteSettingResource.php'),
+            ], 'moonshine');
 
         }
 
