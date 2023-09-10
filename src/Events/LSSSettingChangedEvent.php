7935f60614b9e52c047bd82863e696802351ab85
alexvenga <bubnov.alexey@gmail.com>
2023-09-11 01:39:42 +0300

    Add and dispatch events on change LSSSettingGroupChangedEvent::dispatch(SettingGroup $settingGroup); LSSSettingChangedEvent::dispatch(Setting $setting);

--- src/Events/LSSSettingChangedEvent.php
+++ src/Events/LSSSettingChangedEvent.php
@@ -4,13 +4,13 @@ namespace VI\LaravelSiteSettings\Events;
 
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
-use VI\LaravelSiteSettings\Models\SettingGroup;
+use VI\LaravelSiteSettings\Models\Setting;
 
 class LSSSettingChangedEvent
 {
     use Dispatchable, SerializesModels;
 
-    public function __construct(public SettingGroup $settingGroup)
+    public function __construct(public Setting $setting)
     {
         //
     }
--- src/Events/LSSSettingGroupChangedEvent.php
+++ src/Events/LSSSettingGroupChangedEvent.php
@@ -4,14 +4,13 @@ namespace VI\LaravelSiteSettings\Events;
 
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
-use VI\LaravelSiteSettings\Models\Setting;
 use VI\LaravelSiteSettings\Models\SettingGroup;
 
 class LSSSettingGroupChangedEvent
 {
     use Dispatchable, SerializesModels;
 
-    public function __construct(public Setting $setting)
+    public function __construct(public SettingGroup $settingGroup)
     {
         //
     }
